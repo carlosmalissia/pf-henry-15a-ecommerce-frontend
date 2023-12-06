ff813cc44857cd0e4b2c3364c16c7d9eb0bc753e
Carlos <musicarlos72@gmail.com>
2023-12-06 09:14:40 -0300

    acceso-a-detail

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -13,27 +13,29 @@ export default function Card({
   return (
 
     <div className={`bg-white-500 `} >
-     <div className={style.content__img}>
-            <Image
-              className={style.img}
-              src={images}
-              alt=""
-              width={200}
-              height={300}
-            />
-          </div>             
-    <h3 className="text-bggris items-center text-center ">Ref: {id}</h3> 
-    <div className="flex justify-between w-48">
-        <h2 className="text-center text-black hover:scale-110 items-center">{title}</h2> 
-    </div>
-    <h3 className=" text-bgpriceRed items-center text-center ">Precio: {price} U$</h3>
-    
-    <button className="bg-primary text-white p-3 rounded-lg mx-auto 
+      <Link href={`/Detail/${id}`}>
+        <div className={style.content__img}>
+          <Image
+            className={style.img}
+            src={images}
+            alt=""
+            width={200}
+            height={300}
+          />
+        </div>
+        <h3 className="text-bggris items-center text-center ">Ref: {id}</h3>
+        <div className="flex justify-between w-48">
+          <h2 className="text-center text-black hover:scale-110 items-center">{title}</h2>
+        </div>
+        <h3 className=" text-bgpriceRed items-center text-center ">Precio: {price} U$</h3>
+
+        <button className="bg-primary text-white p-3 rounded-lg mx-auto 
                         flex justify-center items-center text-center hover:bg-bgred
                       hover:text-white transition duration-300">
-      Detalles
-      </button>
-  </div>
+          Detalles
+        </button>
+      </Link>
+    </div>
 
   )
 }
