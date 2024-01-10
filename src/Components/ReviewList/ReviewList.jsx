9ecb08d7a9d056ecbd138d9f5614c28dafb395b4
Pablo91218 <guerre.pablo.agustin@gmail.com>
2024-01-10 15:00:19 -0300

    fecha en las reviews y condicional para el carrito

--- src/Components/ReviewList/ReviewList.jsx
+++ src/Components/ReviewList/ReviewList.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Rating } from '@material-tailwind/react';
+import React, { useEffect, useState } from "react";
+import { Rating } from "@material-tailwind/react";
 
 const ReviewList = ({ productById }) => {
   const [reviews, setReviews] = useState(productById.reviews);
@@ -7,6 +7,12 @@ const ReviewList = ({ productById }) => {
   useEffect(() => {
     setReviews(productById.reviews);
   }, [productById.reviews]);
+
+  const formattedDate = (dateString) => {
+    const options = { year: "numeric", month: "numeric", day: "numeric" };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
+  };
 
   return (
     <div className="w-full p-4 bg-white rounded-md shadow-xl">
@@ -16,12 +22,20 @@ const ReviewList = ({ productById }) => {
       {reviews && reviews.length > 0 ? (
         <ul className="space-y-4">
           {reviews.map((review) => (
-            <li key={review._id} className="rounded-md text-lg">
-              <p>Usuario: {review.user.name}</p>
-              <p className="font-bold text-yellow-500">
-                <Rating readonly value={review.rating} size="sm" />
-              </p>
-              <p>Comentario: {review.comment}</p>
+            <li key={review._id} className="rounded-md text-sm">
+              <div className="flex gap-6 items-center ">
+                <Rating
+                  readonly
+                  value={review.rating}
+                  size="sm"
+                  className="text-yellow-500"
+                  />
+                <p className="text-gray-600 flex items-center text-center" >{formattedDate(review.created)}</p>
+              </div>
+                  <p className="text-gray-600">{review.user.name}</p>
+
+              <p className="text-gray-600"> {review.comment}</p>
+              <br />
             </li>
           ))}
         </ul>
