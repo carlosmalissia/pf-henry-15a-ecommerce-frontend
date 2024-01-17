190e37f82c31667ab20a4a7c49986364941529f8
Pablo91218 <guerre.pablo.agustin@gmail.com>
2024-01-17 14:40:50 -0300

    arreglado(tofixex y bloqueo de perfil)

--- src/app/perfil/page.jsx
+++ src/app/perfil/page.jsx
@@ -1,104 +1,89 @@
-
-
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PerfilUsuario from "@/Components/PerfilUsuario/PerfilUsuario";
 import HistorialCompras from "@/Components/HistorialCompras/HistorialCompras";
-import MisFavoritos from "@/Components/MisFavoritos/MisFavoritos"
 import MisReseñas from "@/Components/MisReseñas/MisReseñas";
 import { useGetUserByIdQuery } from "@/redux/services/usersApi";
 import { useAppSelector } from "@/redux/hooks";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import Link from "next/link";
 import InfoPerfil from "@/Components/InfoPerfil/InfoPerfil";
 import { ToastContainer, toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 export default function perfil() {
+  const userData = useAppSelector((state) => state.loginReducer.user);
+  const router = useRouter();
 
-  //const userData = useAppSelector((state) => state.loginReducer.user);
-  // console.log("userData", userData);
+  useEffect(() => {
+    const checkUserSession = async () => {
+      try {
+        if (!userData) {
+          await router.push("/Register");
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
 
-  //const id = userData?._id
-  // console.log("este es el id " + id);
+    checkUserSession();
+  }, [userData, router]);
+  
 
-
-  const [componenteActual, setComponenteActual] = useState('info');
+  const [componenteActual, setComponenteActual] = useState("info");
 
   const handleClickEnlace = (componente) => {
-    setComponenteActual(componente)
-  }
+    setComponenteActual(componente);
+  };
 
   const renderComponenteActual = () => {
     switch (componenteActual) {
-      case 'informacionPersonal':
+      case "informacionPersonal":
         return <PerfilUsuario />;
-      case 'historialCompras':
+      case "historialCompras":
         return <HistorialCompras />;
-      case 'misReseñas':
+      case "misReseñas":
         return <MisReseñas />;
-      case 'favoritos':
-        return <MisFavoritos />;
-      case 'info':
+      case "info":
         return <InfoPerfil />;
       default:
         return null;
     }
   };
 
-  console.log("estado ", componenteActual);
   return (
     <div className="mt-24 text-center">
-      {/* <h1 className='text-3xl font-semibold mb-4'>Cuenta</h1>
-      <Link href="/" className="text-teal-500 hover:underline">
-        Inicio
-      </Link>
-      <span style={{ color: 'hsl(0, 1%, 32%)' }} className='mx-2'>/</span>
-      <span style={{ color: 'hsl(0, 1%, 32%)' }} className='font-semibold mb-4'>Cuenta</span>
- */}
-
-
-
       <div className="flex ">
         <div className=" shadow-xl border-solid border border-gray-300 w-1/4 h-2/3 mt-20 ml-12 mb-64 p-6 rounded-md">
           <h1 className="mb-10 text-2xl">MI CUENTA</h1>
           <ul>
             <li className="m-2">
               <button
-                className={`cursor-pointer text-xl hover:bg-teal-600  hover:text-white text-black font-serif py-2 px-4 rounded-lg  w-64 shadow-xl ${(componenteActual === "informacionPersonal") ? "bg-teal-600 text-white" : "bg-gray-100"}`}
-                onClick={() => handleClickEnlace('informacionPersonal')}
+                className="cursor-pointer text-xl bg-gray-100 hover:bg-teal-600 text-black font-serif py-2 px-4 rounded-lg  w-64 shadow-xl"
+                onClick={() => handleClickEnlace("informacionPersonal")}
               >
                 Informacion Personal
               </button>
             </li>
             <li className="m-2">
               <button
-                className={`cursor-pointer text-xl hover:bg-teal-600 hover:text-white text-black font-serif py-2 px-4 rounded-lg  w-64 shadow-xl ${(componenteActual === "historialCompras") ? "bg-teal-600 text-white" : "bg-gray-100"}`}
-                onClick={() => handleClickEnlace('historialCompras')}
+                className="cursor-pointer text-xl bg-gray-100 hover:bg-teal-600 text-black font-serif py-2 px-4 rounded-lg  w-64 shadow-xl"
+                onClick={() => handleClickEnlace("historialCompras")}
               >
                 Historial de Compras
               </button>
             </li>
             <li className="m-2">
               <button
-                className={`cursor-pointer text-xl hover:bg-teal-600 hover:text-white text-black font-serif py-2 px-4 rounded-lg  w-64 shadow-xl ${(componenteActual === "misReseñas") ? "bg-teal-600 text-white" : "bg-gray-100"}`}
-                onClick={() => handleClickEnlace('misReseñas')}
+                className="cursor-pointer text-xl bg-gray-100 hover:bg-teal-600 text-black font-serif py-2 px-4 rounded-lg  w-64 shadow-xl"
+                onClick={() => handleClickEnlace("misReseñas")}
               >
                 Mis reseñas
               </button>
             </li>
-            <li className="m-2">
-              <button
-                className={`cursor-pointer text-xl hover:bg-teal-600 hover:text-white text-black font-serif py-2 px-4 rounded-lg  w-64 shadow-xl ${(componenteActual === "favoritos") ? "bg-teal-600 text-white" : "bg-gray-100"}`}
-                onClick={() => handleClickEnlace('favoritos')}
-              >
-                Favoritos
-              </button>
-            </li>
           </ul>
         </div>
-        <div className="w-3/4 m-10 ">
-          {renderComponenteActual()}
-        </div>
+        <div className="w-3/4 m-10 ">{renderComponenteActual()}</div>
         {/* <ToastContainer
                 theme="colored"
                 position="bottom-left"
@@ -108,4 +93,3 @@ export default function perfil() {
     </div>
   );
 }
-
