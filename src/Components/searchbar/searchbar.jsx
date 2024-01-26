c521447ae3776bd525f0ab05d2e91fd20b96ecd0
alanfavatier <alanfavatier305@gmail.com>
2024-01-26 00:55:16 -0300

    search

--- src/Components/searchbar/searchbar.jsx
+++ src/Components/searchbar/searchbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAppSelector } from "@/redux/hooks";
 import { useGetProductByPageQuery } from "@/redux/services/productApi";
+import styles from './Searchbar.module.css';
 
 
 const Searchbar = ({ handlesearchName, category }) => {
@@ -20,31 +21,29 @@ const Searchbar = ({ handlesearchName, category }) => {
   };
 
   return (
-    <div className="flex flex-col items-center max-sm:flex-row max-sm:items-start">
-       <h2 className="mt-12 text-3xl font-custom">
-        Productos 
-      </h2>
-        {/* {category && <i className="ri-arrow-right-line" />} */}
-      {/* {category && <h2 className="mt-2 text-2xl">{category}</h2>} */}
+    
+       
+        
   
-      <div className="mt-8 max-sm:ml-4 max-sm:text-center max-sm:w-full">
-        <form className="flex items-center">
+      <div className={styles.container}>
+        <form className={styles.form}>
           <input
             onChange={handlesearchName}
             type="search"
             placeholder="Buscar..."
-            className="bg-white-500 border-solid border border-gray-300 rounded-md w-[15em] h-[2em] text-center"
+            className={styles.input}
           />
-          <button
+          {/* <button
             className="ml-2 bg-primary p-2 text-white rounded-lg"
             onClick={fetchProducts}
           >
             <i className="ri-refresh-line mr-2" />
             Recargar productos
-          </button>
+          </button> */}
         </form>
       </div>
-    </div>
   );
 };
 export default Searchbar;
+
+
