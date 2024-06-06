eb87b80bc0531c6ad2279b789e1961fde513c14a
Carlos Malissia <116571296+geniototal@users.noreply.github.com>
2024-06-06 13:13:27 -0300

    Add files via upload

---
+++ public/data.js
@@ -0,0 +1,145 @@
+export const data = {
+    products: [
+        {
+            _id: 1,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img1.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 2,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img2.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 3,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img3.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 4,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img4.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 5,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img5.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 6,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img6.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 7,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img7.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 8,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img8.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 9,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img9.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 10,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img10.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 11,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img11.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 12,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img12.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 13,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img13.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+        {
+            _id: 14,
+            title: "Conjunto Camperon-buzo-Lila",
+            price: 39900,
+            image: "/images/img14.jpg",
+            category: "ropa mujer",
+            stock: 30,
+            description:
+                "Conjunto Camperón + Buzo color Lila especial para invierno y zonas frías",
+        },
+
+    ]
+}
