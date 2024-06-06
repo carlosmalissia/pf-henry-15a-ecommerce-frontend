4524dfc261f507bfe415cdf4ace968b385a5fd40
Carlos Malissia <116571296+geniototal@users.noreply.github.com>
2024-06-06 13:33:42 -0300

    Update Banner.jsx

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,8 +4,8 @@ import { RxDotFilled } from "react-icons/rx";
 import Image from "next/image";
 
 function Banner() {
-  const slides = [
-    {
+    const slides = [
+        {
             Image: "/images/banner1.webp",
         },
         {
@@ -25,70 +25,69 @@ function Banner() {
             Image: "/images/banner7.webp",
         }
     ];
-  ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-  const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0;
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
+    const prevSlide = () => {
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    };
 
-  const nextSlide = () => {
-    const isLastSlide = currentIndex === slides.length - 1;
-    const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
+    const nextSlide = () => {
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    };
 
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
-  };
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+    };
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      nextSlide();
-    }, 5000);
-    return () => clearInterval(intervalId);
-  }, [currentIndex]);
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            nextSlide();
+        }, 5000);
+        return () => clearInterval(intervalId);
+    }, [currentIndex]);
 
-  //`url(${slides[currentIndex].Image})`
+    //`url(${slides[currentIndex].Image})`
 
-  return (
-    <div className="max-w-[2000px] mx-auto h-[500px]  group">
-      <Image
-        src={slides[currentIndex].Image}
-        alt="banner"
-        width={1080}
-        height={400}
-        className="w-full h-full object-cover rounded-md"
-      />
-      {/* <div
+    return (
+        <div className="max-w-[2000px] mx-auto h-[500px]  group">
+            <Image
+                src={slides[currentIndex].Image}
+                alt="banner"
+                width={1080}
+                height={400}
+                className="w-full h-full object-cover rounded-md"
+            />
+            {/* <div
         style={{ backgroundImage: `url(${slides[currentIndex].Image})` }}
         className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
       ></div> */}
 
-      {/* Left Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <BsChevronCompactLeft onClick={prevSlide} size={30} />
-      </div>
-      {/* Right Arrow */}
-      <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
-        <BsChevronCompactRight onClick={nextSlide} size={30} />
-      </div>
-      <div className="flex top-4 justify-center py-2">
-        {slides.map((slide, slideIndex) => (
-          <div
-            key={slideIndex}
-            onClick={() => goToSlide(slideIndex)}
-            className="text-2xl cursor-pointer"
-          >
-            <RxDotFilled />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+            {/* Left Arrow */}
+            <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                <BsChevronCompactLeft onClick={prevSlide} size={30} />
+            </div>
+            {/* Right Arrow */}
+            <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+                <BsChevronCompactRight onClick={nextSlide} size={30} />
+            </div>
+            <div className="flex top-4 justify-center py-2">
+                {slides.map((slide, slideIndex) => (
+                    <div
+                        key={slideIndex}
+                        onClick={() => goToSlide(slideIndex)}
+                        className="text-2xl cursor-pointer"
+                    >
+                        <RxDotFilled />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Banner;
