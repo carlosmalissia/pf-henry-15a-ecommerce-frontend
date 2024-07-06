595347fa49d8a3031a1f4688d72917e2e380aef4
Carlos Malissia <116571296+geniototal@users.noreply.github.com>
2024-07-06 13:43:56 -0300

    Update usersApi.js

--- src/redux/services/usersApi.js
+++ src/redux/services/usersApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApi = createApi({
     reducerPath: "usersAPI",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://pf-15a.up.railway.app",
+        baseUrl: "https://api-henrucci.onrender.com",
     }),
     tagTypes: ["Users", 'Reviews'],
     endpoints: (builder) => ({
