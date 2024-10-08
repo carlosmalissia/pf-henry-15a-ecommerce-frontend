cddf5ab8788ac2021a3622f213160d50acd02bb9
Carlos Malissia <116571296+carlosmalissia@users.noreply.github.com>
2024-10-08 17:50:52 -0300

    Update page.jsx

--- src/app/Detail/[_id]/page.jsx
+++ src/app/Detail/[_id]/page.jsx
@@ -221,7 +221,7 @@ export default function DetailID({ params }) {
     <div>
       <div className="bg-bggris2 relative pt-10 mx-auto min-w-[20rem] w-[90%] rounded-2xl flex flex-col md:flex-row mt-28 mb-10 shadow-md">
         {/* Imagen a la izquierda en pantallas grandes */}
-        <div className="bg-white border-solid border-2 border-primary cursor-grab w-[40%] mb-5 mr-10 relative overflow-hidden flex items-center justify-center ml-10">
+        <div className="bg-white border-solid border-2 border-primary cursor-grab w-[40%] mb-5 md:mr-10 relative overflow-hidden flex items-center justify-center md:ml-10">
           <Image
             src={productById.image}
             alt={productById.title}
